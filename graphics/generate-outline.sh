efc02bd1bab3f7fbb490b24ac3f58e605aadf15f
Sebastian Billaudelle <sbillaudelle@gmail.com>
2023-04-03 15:09:01 +0200

    Update for second production run

--- graphics/generate-outline.sh
+++ graphics/generate-outline.sh
@@ -1,2 +1,5 @@
 inkscape neuron.svg -o neuron.eps --export-ignore-filters --export-ps-level=3
 pstoedit -dt -f "dxf:-polyaslines" neuron.eps > neuron.dxf
+
+inkscape qr-code.svg -o qr-code.eps --export-ignore-filters --export-ps-level=3
+pstoedit -dt -f "dxf:-polyaslines" qr-code.eps > qr-code.dxf
